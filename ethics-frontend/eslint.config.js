a746ab75053ac5e81fe970bd450d10bfa919f440
Hariprasad C <haripc525@gmail.com>
2025-03-16 17:02:45 +0530

    Update eslint.config.js

--- ethics-frontend/eslint.config.js
+++ ethics-frontend/eslint.config.js
@@ -4,10 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default [
-  {
-    ignores: ['dist', 'build', 'node_modules', 'eslint.config.js'], // Ensure ESLint ignores itself
-  },
+export default tseslint.config(
+  { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -27,4 +25,4 @@ export default [
       ],
     },
   },
-];
+)
